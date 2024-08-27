d365dab9433cb25bc8d5a25c6fd97d543cd05353
kaiyang <KKYKHAW03.23@ichat.sp.edu.sg>
2024-08-28 06:03:10 +0800

    download and learn basic c programming

---
+++ demo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a =1;
+    int b =2;
+    int c=3;
+    float sum = (float) a/b*c;
+    printf("sdkcmsdmco//cadscc\\qwdqwd%.2f",sum);
+    return 0;
+}
