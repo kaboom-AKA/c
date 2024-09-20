bf31a3ebe9ba46bc3bddfdaeffa603ce9c44efac
kaiyang <KKYKHAW03.23@ichat.sp.edu.sg>
2024-09-21 01:08:41 +0800

    finally completed my programm

--- project1/main.c
+++ project1/main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-#include <stdbool.h> 
+#include <stdlib.h>
+#include <stdbool.h>
 
-// Global variables
 bool start_stop = true;
 
-// Function declarations
-void Hangman();
-void Math();
-void Word_count();
-void ExitProgram();
+void ExitProgram() {
+    char yes_no;
+    printf("\n\nYou sure you want to end the program?\n[type to affirm 'Y']: ");
+    scanf(" %c", &yes_no);
+    if (yes_no == 'Y' || yes_no == 'y') {
+        printf("This is the end of the program.\n");
+        start_stop = false;
+    }
+}
 
 int main() {
-    while (start_stop) 
-    {
+    while (start_stop) {
         int user_choice;
         printf("\tThe Options:\n");
         printf("\t1. Hangman game\n");
@@ -20,16 +23,17 @@ int main() {
         printf("\t3. Word Count\n");
         printf("\t4. Exit Program\n");
         printf("Please Enter Your Input: ");
-        scanf("%d", &user_choice); 
-        switch(user_choice) {
+        scanf("%d", &user_choice);
+
+        switch (user_choice) {
             case 1:
-                Hangman();
+                system("hangman"); // Call Hangman executable
                 break;
             case 2:
-                Math();
+                system("math"); // Call Math executable
                 break;
             case 3:
-                Word_count();
+                system("word_count"); // Call Word Count executable
                 break;
             case 4:
                 ExitProgram();
@@ -39,16 +43,4 @@ int main() {
         }
     }
     return 0;
-}
-
-void ExitProgram() 
-{
-    char yes_no;
-    printf("\n\nYou sure you want to end the program?\n[type to affirm 'Y']: ");
-    scanf(" %c", &yes_no);  
-    if (yes_no == 'Y' || yes_no == 'y') 
-    {  
-        printf("This is the end of the program.\n");
-        start_stop = false;
-    }
 }
